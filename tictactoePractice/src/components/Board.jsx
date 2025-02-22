2024359aec0ed5c7b2d2c08d40ad8544ea86361c
starlord7544 <sidhantsreshtha2003@gmail.com>
2025-02-22 23:36:13 +0530

    Added some css, background, icons etc

--- tictactoePractice/src/components/Board.jsx
+++ tictactoePractice/src/components/Board.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import Cross from '../assets/Cross.svg'
+import Circle from '../assets/Circle.svg'
 
 const Board = ({ boardArr, setBoardArr, turn, setTurn, setDraw, winner, setWinner }) => {
     const winPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
@@ -10,7 +12,7 @@ const Board = ({ boardArr, setBoardArr, turn, setTurn, setDraw, winner, setWinne
     const checkWin = (newBoard) => {
         for (let i = 0; i < winPatterns.length; i++) {
             let [a, b, c] = winPatterns[i]
-            if (newBoard[a] == newBoard[b] && newBoard[b] === newBoard[c] && newBoard[a] !== null)
+            if (newBoard[a] === newBoard[b] && newBoard[b] === newBoard[c] && newBoard[a] !== null)
                 return newBoard[a]
         }
         return null
@@ -33,12 +35,34 @@ const Board = ({ boardArr, setBoardArr, turn, setTurn, setDraw, winner, setWinne
             setDraw(true)
     }
 
+    const giveBorder = (idx) => {
+        let str = 'cell'
+        if (idx === 0 || idx === 1 || idx === 2)
+            str += ' top'
+        if (idx === 6 || idx === 7 || idx === 8)
+            str += ' bottom'
+        if (idx === 0 || idx === 3 || idx === 6)
+            str += ' left'
+        if (idx === 2 || idx === 5 || idx === 8)
+            str += ' right'
+
+        return str
+    }
+
     return (
         <div className="board">
             {
                 boardArr.map((ele, idx) => {
                     return (
-                        <div key={'cell' + idx} className='cell' onClick={() => handleClick(idx)}>{ele}</div>
+                        <div key={'cell' + idx} className={giveBorder(idx)} onClick={() => handleClick(idx)}>
+                            {
+                                ele === 'X' ?
+                                    <img src={Cross} alt='X' className='icon' />
+                                    : ele === 'O' ?
+                                        <img src={Circle} alt='O' className='icon' />
+                                        : ''
+                            }
+                        </div>
                     )
                 })
             }
