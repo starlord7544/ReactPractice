112736519078a0c51cd1dd2b1e4035f6fca12635
starlord7544 <sidhantsreshtha2003@gmail.com>
2025-02-28 13:23:19 +0530

    Fixed winPat bug

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -1,12 +1,42 @@
 import { useState } from 'react'
 import './App.css'
-import TicTacToe from './components/TicTacToe'
+import Board from './components/Board'
 
 function App() {
+    const [turn, setTurn] = useState('X')
+    const [winner, setWinner] = useState(null)
+    const [draw, setDraw] = useState(false)
+    const [boardArr, setBoardArr] = useState(Array(9).fill(null))
+    const [winPat, setWinPat] = useState([])
+    // const winPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+    const handleReset = () => {
+        setBoardArr(Array(9).fill(null))
+        setTurn('X')
+        setDraw(false)
+        setWinner(null)
+        setWinPat([])
+    }
+
     return (
-        <>
-            <TicTacToe />
-        </>
+        <div className="app">
+            <h1>Tic - Tac - Toe</h1>
+            <div className="status">
+                {draw ? 'Draw' : !winner ? `${turn}'s turn` : `Player ${winner} Won`}
+            </div>
+            <Board
+                turn={turn}
+                setTurn={setTurn}
+                setDraw={setDraw}
+                boardArr={boardArr}
+                setBoardArr={setBoardArr}
+                winner={winner}
+                setWinner={setWinner}
+                winPat={winPat}
+                setWinPat={setWinPat}
+            />
+            <button className='reset-btn' onClick={handleReset}>Reset</button>
+        </div>
     )
 }
 
---
+++ TicTacToe/src/components/Board.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import Cross from '../assets/Cross.svg'
+import Circle from '../assets/Circle.svg'
+
+const Board = ({ boardArr, setBoardArr, turn, setTurn, setDraw, winner, setWinner, winPat, setWinPat }) => {
+    const winPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    const checkDraw = (newBoard) => {
+        return newBoard.every(cell => cell !== null)
+    }
+
+    const checkWin = (newBoard) => {
+        for (let i = 0; i < winPatterns.length; i++) {
+            let [a, b, c] = winPatterns[i]
+            if (newBoard[a] === newBoard[b] && newBoard[b] === newBoard[c] && newBoard[a] !== null) {
+                setWinPat(winPatterns[i])
+                return newBoard[a]
+            }
+        }
+        return null
+    }
+
+    const handleClick = (idx) => {
+        if (boardArr[idx] !== null || winner)
+            return
+        const newBoard = [...boardArr]
+        newBoard[idx] = turn
+        setBoardArr(newBoard)
+        setTurn(prev => prev === 'X' ? 'O' : 'X')
+
+        const winPlayer = checkWin(newBoard)
+
+        if (winPlayer !== null) {
+            setWinner(winPlayer)
+        }
+        else if (checkDraw(newBoard))
+            setDraw(true)
+    }
+
+    const giveBorder = (idx) => {
+        let str = 'cell'
+        if (winPat.length !== 0) {
+            for (let i = 0; i < winPat.length; i++)
+                if (winPat[i] === idx)
+                    str += ' win'
+        }
+        if (idx === 0 || idx === 1 || idx === 2)
+            str += ' top'
+        if (idx === 6 || idx === 7 || idx === 8)
+            str += ' bottom'
+        if (idx === 0 || idx === 3 || idx === 6)
+            str += ' left'
+        if (idx === 2 || idx === 5 || idx === 8)
+            str += ' right'
+
+        return str
+    }
+
+    return (
+        <div className="board">
+            {
+                boardArr.map((ele, idx) => {
+                    return (
+                        <div key={'cell' + idx} className={giveBorder(idx)} onClick={() => handleClick(idx)}>
+                            {
+                                ele === 'X' ?
+                                    <img src={Cross} alt='X' className='icon' />
+                                    : ele === 'O' ?
+                                        <img src={Circle} alt='O' className='icon' />
+                                        : ''
+                            }
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Board
